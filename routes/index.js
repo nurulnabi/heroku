1ef17a04e6de455686297fe63887ddd50be38790
noor <noorul.ansari@comprotechnologies.com>
2018-05-04 16:42:44 +0530

    new end point /me added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello There! This is Noor' });
 });
 
+router.get("/me", function(req, res, next){
+    res.send({name:'NOOR', age:24});
+    console.log({name:'NOOR', age:24})
+})
+
 module.exports = router;
